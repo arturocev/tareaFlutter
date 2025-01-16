88772c8322be9050b77e33394a7a2d7b42e96731
Arturo alumno 516 <arturo_garcia_m-tsapp1oa2324@cev.com>
2025-01-16 13:36:33 +0100

    Pantalla listado terminada

--- lib/listado.dart
+++ lib/listado.dart
@@ -1,13 +1,7 @@
 import 'dart:convert';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:tarea_flutter/personaje.dart';
-
-  void main() 
-  {
-    runApp(const listadoApp());
-  }
 
 class listadoApp extends StatefulWidget {
   const listadoApp({super.key});
@@ -18,48 +12,59 @@ class listadoApp extends StatefulWidget {
 
 class _listadoAppState extends State<listadoApp> {
 
-  late String texto;
+  late List<String> personajes;
+  late int counter;
 
   @override
   void initState()
   {
-    texto = "0";
-    aniadirAmigos();
+    personajes = [];
+    counter = 0;
+    listadoPersonajes();
     super.initState();
   }
 
-  void aniadirAmigos() async
-  {
-    final url = Uri.parse("https://anapioficeandfire.com/api/characters/583");
-    final response = await http.get(url);
-    if (response.statusCode == 200) 
-    {
-      final json = response.body;
-      Personaje personaje = Personaje.fromJson(jsonDecode(json));
-      texto = personaje.name;
-    }
-    else
-    {
-      texto = "No ha funcionado";
+  void listadoPersonajes() async {
+    while (counter < 100) {
+      counter++;
+      final url = Uri.parse("https://www.anapioficeandfire.com/api/characters/$counter");
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final json = response.body;
+        Personaje personaje = Personaje.fromJson(jsonDecode(json));
+        if (personaje.name.isEmpty) {
+          personajes.add("Personaje $counter");
+        }
+        else {
+          personajes.add(personaje.name);
+        }
+
+      }
     }
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-  return ListView.builder(
-    padding: const EdgeInsets.all(8),
-    itemCount: entries.length,
-    itemBuilder: (BuildContext context, int index) {
-      return Container(
-        height: 50,
-        child: Center(child: Text('$texto')),
+    return ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: personajes.length,
+        itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          child: Center(child: 
+          Text(
+            personajes[index],
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.black
+            ),
+            ),
+            ),
+         );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       );
-    }
-  );
+  }
 }
-}
-
-final List<String> entries = <String>['A', 'B', 'C'];
-final List<int> colorCodes = <int>[600, 500, 100];
-
